0b0f2177e5137ebf437eb7b08643c6f0a35b04f8
Aleena Khan <khanaleena0709@gmail.com>
2026-07-22 20:52:49 +0500

    feat:implement homepage with dynamic task listing

--- tasks/views.py
+++ tasks/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,"tasks/home.html")
+    tasks = [
+        "Learn Django",
+        "Learn Git",
+        "Build Portfolio",
+    ]
+
+    context={
+        "tasks":tasks
+    }
+    return render(request,"tasks/home.html",context)
